6f10c2703a5c65bc8a9a1d8ee2ea6b0637207aae
zhqzhq <2847492481@qq.com>
2024-06-24 22:10:17 +0800

    sort items and clear items

--- 03-trip-list/src/App.js
+++ 03-trip-list/src/App.js
@@ -19,6 +19,10 @@ function App() {
     );
   }
 
+  function handleClearItems() {
+    setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,6 +31,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onUpdateItem={handleUpdateItem}
+        onClearList={handleClearItems}
       />
       <Stats items={items} />
     </div>
@@ -76,11 +81,24 @@ function From({ onAddItem }) {
   );
 }
 
-function List({ items, onDeleteItem, onUpdateItem }) {
+function List({ items, onDeleteItem, onUpdateItem, onClearList }) {
+  const [sortMethod, setSortMethod] = useState("inputOrder");
+  let sortedItems;
+  if (sortMethod === "inputOrder") sortedItems = items;
+  if (sortMethod === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortMethod === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -89,6 +107,18 @@ function List({ items, onDeleteItem, onUpdateItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select
+          value={sortMethod}
+          onChange={(e) => setSortMethod(e.target.value)}
+        >
+          <option value="inputOrder">Sort By Input Order</option>
+          <option value="description">Sort By description</option>
+          <option value="packed">Sort By packed</option>
+        </select>
+        <button onClick={() => onClearList()}>Clear List</button>
+      </div>
     </div>
   );
 }
