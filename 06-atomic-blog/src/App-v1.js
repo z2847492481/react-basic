1eaee2d79a6b237c817c556e4ac0a927aa5980c1
zhqzhq <2847492481@qq.com>
2024-08-10 16:12:08 +0800

    refactor postcontext

--- 06-atomic-blog/src/App-v1.js
+++ 06-atomic-blog/src/App-v1.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
-import { PostProvider, usePosts } from "./PostContext";
 
 function createRandomPost() {
   return {
@@ -9,8 +8,32 @@ function createRandomPost() {
   };
 }
 
+const PostContext = createContext();
+
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -21,25 +44,34 @@ function App() {
   );
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
-      <PostProvider>
+    <PostContext.Provider
+      value={{
+        searchedPosts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        searchQuery: searchQuery,
+        setSearchQuery: setSearchQuery,
+        onAddPost: handleAddPost,
+      }}
+    >
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
+
         <Header />
         <Main />
-        <Archive />
+        <Archive onAddPost={handleAddPost} />
         <Footer />
-      </PostProvider>
-    </section>
+      </section>
+    </PostContext.Provider>
   );
 }
 
 function Header() {
-  const { onClearPosts } = usePosts();
+  const { onClearPosts } = useContext(PostContext);
   return (
     <header>
       <h1>
@@ -55,7 +87,7 @@ function Header() {
 }
 
 function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePosts();
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
   return (
     <input
       value={searchQuery}
@@ -69,7 +101,7 @@ function SearchPosts() {
 }
 
 function Results() {
-  const { searchedPosts } = usePosts();
+  const { searchedPosts } = useContext(PostContext);
   return <p>🚀 {searchedPosts.length} atomic posts found</p>;
 }
 
@@ -91,7 +123,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-  const { onAddPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
@@ -121,7 +153,7 @@ function FormAddPost() {
 }
 
 function List() {
-  const { searchedPosts } = usePosts();
+  const { searchedPosts } = useContext(PostContext);
   return (
     <ul>
       {searchedPosts.map((post, i) => (
@@ -135,7 +167,7 @@ function List() {
 }
 
 function Archive() {
-  const { onAddPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
